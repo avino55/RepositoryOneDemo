5431030e9ef69e276400bff1138b7dd0cf0f2603
Anthony Vino <avino55@gmail.com>
2025-05-24 12:05:15 -0400

    Commited in GitB at 12:05

--- pythonTesting/pythonBasics/FirstDemo.py
+++ pythonTesting/pythonBasics/FirstDemo.py
@@ -8,7 +8,9 @@ Str = "Hello World"
 print(Str)
 
 b, c, d = 5, 6.4, "Great"
-
+git_TEST1 = "Entered in the GITB 1"
+git_TEST2 = "Entered in the GITB 2"
+git_TEST3 = "Entered in the GITB 3"
 #print("Value is"+b+"fdfd")
 
 
